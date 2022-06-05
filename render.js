a3d6ef9f3b2376e6a5ecfd5b56c108da4a14ddfe
prismquiz-ru <106191023+prismquiz-ru@users.noreply.github.com>
2022-06-05 23:46:06 +0500

    Update render.js

--- render.js
+++ render.js
@@ -11,7 +11,7 @@ function renderFormat() {
     }
     bl = [100, Y, Boxheight, Boxheight]
     leftbox = createBox(bl).attr('fill','#ffff7f')
-    br = [1200-Boxheight, Y, Boxheight, Boxheight]
+    br = [1000-Boxheight, Y, Boxheight, Boxheight]
     rightbox = createBox(br).attr('fill','#30b364')
     textg = svg.append('g');
     for(var i = 0; i < maxQs; i++){
@@ -80,7 +80,7 @@ function renderQuestion(selected){
         if(selected >= 0) {
             createLineText([CW-H/2,CH-W2/2,H,W2],LANGUAGES["BL"][curlang]).attr("transform","rotate(-90,"+CW+","+CH+")").attr('fill','lightgrey')
             svg.append("image").attr("href","data/left.png").attr('x',100-W).attr('y',80).attr('width',W).attr('height',H)
-            createLineText([1000-(CW+H/2),CH-W2/2,H,W2],LANGUAGES["BR"][curlang]).attr("transform","rotate(90,"+(1000-CW)+","+CH+")").attr('fill','lightgrey')
+            createLineText([1100-(CW+H/2),CH-W2/2,H,W2],LANGUAGES["BR"][curlang]).attr("transform","rotate(90,"+(1000-CW)+","+CH+")").attr('fill','lightgrey')
             svg.append("image").attr("href","data/right.png").attr('x',1000).attr('y',80).attr('width',W).attr('height',H)
         } else {
             //bounds: [10,80,85-W,80,H]
